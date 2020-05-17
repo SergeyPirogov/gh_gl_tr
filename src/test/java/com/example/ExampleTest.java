13e7847beb0ab2d952f50a3ed2ff4a6b8859dfe8
SergeyPirogov <semen4ik20@gmail.com>
2020-05-17 14:38:58 +0300

    add UI test

--- src/test/java/com/example/ExampleTest.java
+++ src/test/java/com/example/ExampleTest.java
@@ -1,11 +1,16 @@
 package com.example;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 import org.junit.Test;
+
+import static com.codeborne.selenide.Selenide.$;
 
 public class ExampleTest {
 
     @Test
     public void name() {
-
+        Selenide.open("https://angular.realworld.io/");
+        $("body > app-root > app-layout-header > nav > div > ul > li:nth-child(2) > a").shouldHave(Condition.text("Sign in"));
     }
 }
