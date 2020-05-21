2f22f694491c298bfa6f66abdb6fc0cd82d7d2d5
SergeyPirogov <semen4ik20@gmail.com>
2020-05-21 09:09:38 +0300

    add wd provider

--- src/test/java/com/example/WebdriverManager.java
+++ src/test/java/com/example/WebdriverManager.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import com.codeborne.selenide.WebDriverProvider;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -13,6 +14,7 @@ public class WebdriverManager implements WebDriverProvider {
         options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
+        WebDriverManager.chromedriver().setup();
         return new ChromeDriver(options);
     }
 }
